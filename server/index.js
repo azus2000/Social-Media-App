ee34c0a6474ce88627ae9996a54a6c79fc128084
Ayush Ranjan <ayushranjan23@gmail.com>
2022-11-14 14:33:10 +0530

    Add existing file

--- server/index.js
+++ server/index.js
@@ -11,9 +11,12 @@ app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use(cors());
 app.use('/posts',postRoutes)
-
-const CONNECTION_URL = 'mongodb+srv://jayeshpaliwal:jayesh210@cluster0.u4hekqt.mongodb.net/?retryWrites=true&w=majority';
+app.get('/',(req,res)=>{
+    res.send('Hello World!');
+})
+const CONNECTION_URL = 'mongodb+srv://ayushranjan23:ayushranjan23@cluster0.anyjkrs.mongodb.net/?retryWrites=true&w=majority';
 const PORT=process.env.PORT || 5000;
+
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(() => app.listen(PORT,()=>console.log("Server running on port: 5000")))
 .catch((error)=>console.log(error.message))
